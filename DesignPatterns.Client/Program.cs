eedfd5528c0e976f530cba98f946b30047ebea20
Matt Harp <wildharpo@yahoo.com>
2024-09-16 19:13:12 -0500

    Initial check in

--- DesignPatterns.Client/Program.cs
+++ DesignPatterns.Client/Program.cs
@@ -3,4 +3,4 @@ using DesignPatterns.Library.Polymorphism;
 
 var payments = new PaymentBase[] { new CreditCard(100), new Paypal(200) };
 foreach (var payment in payments)
-    payment.ProcessPayment();
+    payment.ProcessPayment(); 
